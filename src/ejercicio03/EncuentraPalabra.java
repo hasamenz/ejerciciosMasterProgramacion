ce0c68eb06153419ba3c5f7de68a1b8e77d9ade1
Hugo <you@example.com>
2023-11-10 10:53:48 -0500

    Subiendo parte del ejercicio 3

---
+++ src/ejercicio03/EncuentraPalabra.java
@@ -0,0 +1,18 @@
+package ejercicio03;
+
+import java.util.Scanner;
+
+public class EncuentraPalabra {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese la frase : ");
+        Encuentrafrase("esta es una frase de la mañana","manana");
+
+    }
+
+    public static void Encuentrafrase(String frase, String palabra){
+        
+
+    }
+
+}
