9ac7b7a83d6204a400fae39867cc400c0aaa06e5
Hugo <you@example.com>
2023-11-20 12:04:28 -0500

    Subiendo ejercicio numero 3 terminado

--- src/ejercicio03/EncuentraPalabra.java
+++ src/ejercicio03/EncuentraPalabra.java
@@ -3,16 +3,23 @@ package ejercicio03;
 import java.util.Scanner;
 
 public class EncuentraPalabra {
+    //Dada una palabra, buscarla en una frase y devolver cuantas vees aparece esa palabra en la frase escrita.
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Ingrese la frase : ");
-        Encuentrafrase("esta es una frase de la mañana","manana");
+
+        System.out.println(Encuentrafrase("caballero de fina estampa mucho bello caballero en el rancho caballero caballeros","caballero"));
 
     }
-
-    public static void Encuentrafrase(String frase, String palabra){
-        
-
+    public static int Encuentrafrase(String frase, String palabra){
+        String[] arrayFrase = frase.split(" ");
+        int contador = 0;
+        for (int i = 0; i < arrayFrase.length; i++){
+            if (arrayFrase[i].equals(palabra)){
+                contador++;
+            }
+        }
+        return contador;
     }
 
 }
