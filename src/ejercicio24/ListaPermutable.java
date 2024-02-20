de535689ee39823897a7e2a5e18716bce414be1d
Hugo <shugo.alva20@gmail.com>
2024-02-20 12:26:26 -0500

    avance de ejercicio 24

--- src/ejercicio24/ListaPermutable.java
+++ src/ejercicio24/ListaPermutable.java
@@ -2,11 +2,19 @@ package ejercicio24;
 
 public class ListaPermutable {
     public static void main(String[] args) {
-
+        calcularLista(new int[]{4, 5, 8, 7},7);
     }
 
     public static void calcularLista(int[]listaNumeros, int numeroFinal){
+        int contador = 0;
+        for (int i = 0; i < listaNumeros.length; i++){
 
+            if (listaNumeros[i] == contador){
+                System.out.println(listaNumeros[i] + "Si es correlativo");
+                contador++;
+            }
+
+        }
     }
 
 }
