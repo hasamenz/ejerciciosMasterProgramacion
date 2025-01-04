2d2398470c22321ef8c1d3706d45bc403c658a8f
hasamenz <shugo.alva20@gmail.com>
2025-01-04 18:30:56 -0500

    probando

--- src/ejercicio31/MedidaAngulos.java
+++ src/ejercicio31/MedidaAngulos.java
@@ -3,6 +3,9 @@ package ejercicio31;
 public class MedidaAngulos {
     public static void main(String[] args) {
         System.out.println("probando subida desde linux");
+        System.out.println("probando subida desde linux");
+        System.out.println("probando subida desde linux");
+        System.out.println("probando subida desde linux");
 
     }
 }
